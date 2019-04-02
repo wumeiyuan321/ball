0cd994966d157e2c68e0001c00a33c9ce90913d0
wumeiyuan321 <49157257+wumeiyuan321@users.noreply.github.com>
2019-04-02 22:13:38 +0800

    Add files via upload

---
+++ ball.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+    int x=10;
+    int y=20;
+    for(i=0;i<x;i++)
+		printf("\n");
+	for(j=0;j<y;j++)
+		printf(" ");
+	printf("o");
+	printf("\n");
+}
