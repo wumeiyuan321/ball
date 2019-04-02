f8b7076063ba858ecd153169dd34814bdfd8165b
wumeiyuan321 <49157257+wumeiyuan321@users.noreply.github.com>
2019-04-02 22:38:32 +0800

    Add files via upload

---
+++ ball1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int i,j;
+    int x=1;
+    int y=8;
+	for(x=1;x<8;x++)
+	{
+		system("cls");
+
+		for(i=0;i<x;i++)
+		   printf("\n");
+	
+		for(j=0;j<y;j++)
+		   printf(" ");
+	    printf("o");
+	    printf("\n"); 
+	}
+    
+}
